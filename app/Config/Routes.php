801bf8c4c90a4275de4f5abb558f1b2434ceec02
victor fernando <victorfernandomagalhaes@gmail.com>
2023-10-30 19:09:41 -0300

    fix

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -18,7 +18,7 @@ $routes->group('clients', function ($routes) {
 $routes->group('inspections', function ($routes) {
     $routes->post('getInspectableList', 'InspectionController::getInspectableList', ['filter' => 'authFilter']);
     $routes->post('get_maintenance_type', 'InspectionController::getMaintenanceType', ['filter' => 'authFilter']);
-    $routes->put('alter_status/(:any)', 'InspectionController::updateInspectionStatusById/$1', ['filter' => 'authFilter']);
+    $routes->put('alter_status', 'InspectionController::updateInspectionStatusById', ['filter' => 'authFilter']);
     $routes->post('save_is_closed', 'InspectionController::saveInspectableIsClosed', ['filter' => 'authFilter']);
     $routes->post('register_maintenance', 'InspectionController::registerMaintenance', ['filter' => 'authFilter']);
     $routes->get('(:any)', 'InspectionController::getInspectionsByClientIdAndStatus/$1', ['filter' => 'authFilter']);
